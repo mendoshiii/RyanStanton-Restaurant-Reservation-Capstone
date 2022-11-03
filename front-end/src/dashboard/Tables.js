72b62736e991eb64736482fbaff163efab2c7728
Ryan Stanton <ryanstanton9123@gmail.com>
2022-11-03 12:32:24 -0700

    All backend and frontend tests passing 01-05

--- front-end/src/dashboard/Tables.js
+++ front-end/src/dashboard/Tables.js
@@ -4,6 +4,18 @@ import { finishTable } from "../utils/api";
 export default function Tables({ table, loadDashboard }) {
   const { table_id, table_name, capacity, reservation_id } = table;
 
+  const handleFinish = () => {
+    const confirmBox = window.confirm(
+      "Is this table ready to seat new guests? This cannot be undone."
+    );
+    if (confirmBox === true) {
+      finishTable(table_id)
+        .then(loadDashboard)
+        .catch((error) => console.log(error));
+    }
+    return null;
+  };
+
   return (
     <tr key={table_id}>
       <th scope="row">{table_id}</th>
@@ -17,14 +29,7 @@ export default function Tables({ table, loadDashboard }) {
           <button
             data-table-id-finish={table_id}
             className="delete button btn btn-warning"
-            onClick={() => {
-              const confirmBox = window.confirm(
-                "Is this table ready to seat new guests? This cannot be undone."
-              );
-              if (confirmBox === true) {
-                finishTable(table_id);
-              }
-            }}
+            onClick={handleFinish}
           >
             Finish
           </button>
